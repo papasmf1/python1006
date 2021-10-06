dddbbe7e392d826fbf791888f2d12567c7d8d9d9
papasmf1 <papasmf1@gmail.com>
2021-10-06 16:36:42 +0900

    클래스 연습5

--- isinstace함수_파생여부확인.py
+++ isinstace함수_파생여부확인.py
@@ -1,11 +1,17 @@
 class Person:
     pass
+
 class Bird:
     pass
+
+#상속을 연습 
 class Student(Person):
     pass
 
+#인스턴스를 나열해서 생성 
 p, s = Person(), Student()
+
+#전역 함수 
 print("p is instance of Person: ", isinstance(p, Person))
 print("s is instance of Person: ", isinstance(s, Person))
 print("p is instance of Object: ", isinstance(p, object))
--- 생성자소멸자_참조카운트.py
+++ 생성자소멸자_참조카운트.py
@@ -1,17 +1,20 @@
 # -*- 생성자와 소멸자 그리고 참조 카운트 관리  -*-
 class MyClass:
+    #생성자 메서드 
     def __init__(self, value):
         self.value = value
         print("Instance is created! Value = ", value)
+    #소멸자 메서드 
     def __del__(self):
         print("Instance is deleted!")
 
+
 #인스턴스 생성 
 d = MyClass(10)
-d_copy = d
-del d_copy 
-del d
-
+#인스턴스를 다 사용했으면 삭제도 가능 
+#del d
+#자동으로 가비지 컬렉션(GC) 서비스를 제공 
+print("전체 코드 실행 종료")
 
 
 
