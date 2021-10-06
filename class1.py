db6fe9951f9df519cf0831682ea5642d0df5639a
papasmf1 <papasmf1@gmail.com>
2021-10-06 14:38:08 +0900

    클래스 연습2

--- class1.py
+++ class1.py
@@ -12,5 +12,14 @@ class Person:
 #인스턴스 생성
 p1 = Person()
 p1.print()
+p2 = Person()
+p2.name = "전우치"
+p2.print()
 
-
+#런타임시에 변수가 추가됨
+#런타임은 코드가 실행되고 있다는 의미 
+#디자인타임은 코딩을 하고 있다. 
+Person.title = "new title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
