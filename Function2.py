61d5261f1fbcf6b7b160c7b33c067fa2df242611
papasmf1 <papasmf1@gmail.com>
2021-10-06 11:40:14 +0900

    함수연습3

--- Function2.py
+++ Function2.py
@@ -14,3 +14,47 @@ change(wordlist)
 print("함수 호출후:", wordlist)
 
 
+#global키워드 사용 
+#전역변수로 초기화
+g = 1 
+def testScope(a):
+    #불변형식인 전역변수를 읽기 + 쓰기 가능 
+    global g 
+    g = 2 
+    return a+g 
+
+#함수 호출
+print( testScope(1) )
+print("전역변수 g:", g)
+
+
+#기본값을 사용
+def times(a=10, b=20):
+    return a*b 
+
+#호출
+print( times() )
+print( times(5) )
+print( times(5,6) )
+
+#키워드 인자 방식(파라메터명 기술)
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port 
+    return strURL 
+
+#호출
+print( connectURI("credu.com", "80") )
+print( connectURI(port="80", server="credu.com") )
+
+#가변인자
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result 
+
+#호출
+print( union("HAM","EGG") )
+print( union("HAM","EGG","SPAM") )
