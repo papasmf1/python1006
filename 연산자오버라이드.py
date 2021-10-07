ea37dd51364bb2ee12885392ac820c133a9e8d77
papasmf1 <papasmf1@gmail.com>
2021-10-07 09:31:29 +0900

    연산자 오버로드

--- 연산자오버라이드.py
+++ 연산자오버라이드.py
@@ -1,15 +1,17 @@
-#연산자 오버라이드 
+#연산자 오버로드 
 class NumBox:
 	def __init__(self, num):
 		self.Num = num
-	def __add__(self, num):
+	# + 연산자(object클래스 정의)
+	def add(self, num):
 		self.Num += num
-	def __sub__(self, num):
+	# - 연산자의 의미를 정의 
+	def remove(self, num):
 		self.Num -= num
 
 #인스턴스 생성
 n = NumBox(40)
-n + 100 
+n.add(100)
 print(n.Num)
-n - 110 
+n.remove(30)
 print(n.Num)
